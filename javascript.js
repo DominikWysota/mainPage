22633400f20845b6fbcc2e235fd271313458caea
Dominik Wysota <Medutenza@gmail.com>
2019-06-05 16:40:03 +0200

    Bad javascript file

--- javascript.js
+++ javascript.js
@@ -1,51 +1,21 @@
-$('.menu_mobile, .hide').hide();
+$('.about, .skill, .project, .contact').hide();
 
 $(document).ready(function () {
 	$('nav').click(function () {
 		$('nav').toggleClass('active');
-		$('.menu_mobile').animate({
+		$('#baner').toggleClass('active');
+		$('#groupone').toggleClass('active');
+		$('.about').animate({
 			width: 'toggle'
 		}, 300);
-		$('.hide').animate({
-			visibility: 'toggle'
+		$('.skill').delay(300).animate({
+			width: 'toggle'
 		});
-		$('body').toggleClass('stop-scrolling')
-	});
-});
-
-var size = 0;
-var price = document.getElementById("price").innerHTML;
-
-$(document).ready(function(){
-    $('.minus').click(function (){
-        if (size > 0){
-            size--;
-            size_pizza();
-        }
-    });
-    $('.plus').click(function (){
-        if (size < 3){
-            size++;
-            size_pizza();
-        }
-    });
-});
-
-function size_pizza(){
-if (size == 0){
-        document.getElementById("js_size").innerHTML = "Small";
-        document.getElementById("price").innerHTML = price;
-    }
-if (size == 1){
-        document.getElementById("js_size").innerHTML = "Medium";
-        document.getElementById("price").innerHTML = (price * 0.2 + price * 1).toFixed(2);
-    }
-if (size == 2){
-        document.getElementById("js_size").innerHTML = "Large";
-        document.getElementById("price").innerHTML = (price * 0.5 + price * 1).toFixed(2);
-    }
-if (size == 3){
-        document.getElementById("js_size").innerHTML = "Party";
-        document.getElementById("price").innerHTML = (price * 0.8 + price * 1).toFixed(2);
-    }
-};
+		$('.project').delay(600).animate({
+			width: 'toggle'
+		});
+		$('.contact').delay(900).animate({
+			width: 'toggle'
+		});
+	})
+})
